e7fe976adbfb132fceab5a933dbe27db14aaef3f
Crowdbotics <team@crowdbotics.com>
2023-09-09 15:38:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-star-43302",
+  url: "https://floral-star-43302.botics.co",
+  api: "https://floral-star-43302.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
